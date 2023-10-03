5e0ab400e302e2a44d2bcca18fc38121404bf403
Sangin Geum <56298463+sangingeum@users.noreply.github.com>
2023-10-04 02:34:39 +0900

    File transfer example
    File transfer example

--- FileTransmitClient/main.cpp
+++ FileTransmitClient/main.cpp
@@ -3,18 +3,28 @@
 #include <iostream>
 #include <array>
 
+// client
 using namespace asio::ip;
 //127.0.0.1
 asio::io_service service;
 tcp::socket tcpSocket{ service };
+std::array<char8_t, 4096> data;
+//std::ofstream file;
+std::basic_ofstream<char8_t, std::char_traits<char8_t>> file;
 
+void readHandler(const asio::error_code& code, size_t bytesTransferred) {
 
+	if (!code) {
+		file.write(data.data(), bytesTransferred);
+		tcpSocket.async_read_some(asio::buffer(data, data.size()), readHandler);
+	}
+	
+}
 
 void connectHandler(const asio::error_code& code) {
 	if (!code) {
-		std::cout << "hi\n";
+		tcpSocket.async_read_some(asio::buffer(data, data.size()), readHandler);
 	}
-	std::cout << code.message();
 }
 
 void resolverHandler(const asio::error_code& code, tcp::resolver::iterator it) {
@@ -25,11 +35,17 @@ void resolverHandler(const asio::error_code& code, tcp::resolver::iterator it) {
 
 
 int main() {
-	tcp::resolver resolver{ service };
-	tcp::resolver::query query{ "127.0.0.1", "13579" };
-	resolver.async_resolve(query, resolverHandler);
-
-	service.run();
-
+	file.open("fromClient.txt", std::ios::trunc | std::ios::binary);
+	if (file) {
+		tcp::resolver resolver{ service };
+		tcp::resolver::query query{ "127.0.0.1", "13579" };
+		resolver.async_resolve(query, resolverHandler);
+		service.run();
+		std::cin.get();
+	}
+	else {
+		std::cout << "cannot open file\n";
+	}
+	
 	return 0;
 }
--- FileTransmitServer/main.cpp
+++ FileTransmitServer/main.cpp
@@ -2,29 +2,63 @@
 #include <fstream>
 #include <iostream>
 #include <array>
+#include <string>
 
+// server
 using namespace asio::ip;
 //127.0.0.1
 asio::io_service service;
 tcp::socket tcpSocket{ service };
 tcp::endpoint tcp_endpoint{ tcp::v4(), 13579 };
 tcp::acceptor tcpAccepter{ service , tcp_endpoint };
+std::ifstream file2;
+std::basic_ifstream<char8_t, std::char_traits<char8_t>> file;
+std::array<char8_t, 4096> data;
 
 
+void writeHandler(const asio::error_code& code, size_t bytesTransferred) {
+	if (!code) {
+		if (file) {
+			file.read(data.data(), data.size());
+			size_t bytesRead = file.gcount();
+			if (bytesRead > 0)
+				tcpSocket.async_write_some(asio::buffer(data, bytesRead), writeHandler);
+			else
+				tcpSocket.shutdown(tcp::socket::shutdown_send);
+		}
+	}
+	else {
+		tcpSocket.shutdown(tcp::socket::shutdown_send);
+	}
+}
 
 void acceptHandler(const asio::error_code& code) {
 	if (!code) {
-		std::cout << "hi\n";
+		if (file) {
+			file.read(data.data(), data.size());
+			size_t bytesRead = file.gcount();
+			if(bytesRead > 0)
+				tcpSocket.async_write_some(asio::buffer(data, bytesRead), writeHandler);
+			else
+				tcpSocket.shutdown(tcp::socket::shutdown_send);
+		}
+		else {
+			tcpSocket.shutdown(tcp::socket::shutdown_send);
+		}
 	}
 }
 
 
 int main() {
-
-	tcpAccepter.listen();
-	tcpAccepter.async_accept(tcpSocket, acceptHandler);
-
-	service.run();
-
+	file.open("fromServer.txt", std::ios::_Nocreate | std::ios::binary);
+	if (file) {
+		tcpAccepter.listen();
+		tcpAccepter.async_accept(tcpSocket, acceptHandler);
+		service.run();
+	}
+	else {
+		std::cout << "Cannot open fromServer.txt\n" << "\n";
+	}
+	std::cin.get();
 	return 0;
 }
