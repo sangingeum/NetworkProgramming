9d1e3e2f195f1312c03af834299b89e9a04c8ccd
Sangin Geum <56298463+sangingeum@users.noreply.github.com>
2023-10-25 17:29:18 +0900

    Add comments
    Add comments

--- FileTransfer/FileTransferClient/main.cpp
+++ FileTransfer/FileTransferClient/main.cpp
@@ -3,6 +3,8 @@
 
 int main() {
 	try {
+		// This asynchronous file transfer client tries to connect to the endpoint ("127.0.0.1", "13579").
+		// After it receives a file from the server, it terminates.
 		asio::io_service service;
 		FileTransferClient client{ service };
 		client.asyncGetFile("client.txt", "127.0.0.1", "13579");
--- FileTransfer/FileTransferServer/main.cpp
+++ FileTransfer/FileTransferServer/main.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 int main() {
 	try {
+		// This asynchronous file transfer server listens on port 13579.
+		// This server continuously transfers the 'server.txt' file to any clients
+		// that connect to it using the Asio library.
 		asio::io_service service;
 		FileTransferServer server{ service , 13579 };
 		server.start("server.txt");
